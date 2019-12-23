6018aa12e8cec09968fbe0c9e4d35f93bd07237c
Şahin <sahinbesinci@gmail.com>
2019-12-23 14:58:00 +0300

    Update 280020.cpp

--- 280020.cpp
+++ 280020.cpp
@@ -669,7 +669,7 @@ int main()
 			cout<<t2.itemAt(1,1)<<endl; // (1,1) deki eleman
 			cout<<t2.itemAt("A2")<<endl;  // (2,0) deki eleman
 			string s2[] = {"S1","S2","S3"};
-			t1.setColNames(s2,3);
+			t2.setColNames(s2,3);
 			cout<<t2.itemAt("1","S2")<<endl;  // (1,1) deki eleman
 		}
 		break;
